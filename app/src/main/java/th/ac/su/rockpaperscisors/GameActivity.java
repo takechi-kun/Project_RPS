34e64b78175c0baca2a64e6461cb9674098290a5
Peerasit Suntieakchun <peerasit11@gmail.com>
2020-11-01 03:20:13 +0700

    Initial commit

---
+++ app/src/main/java/th/ac/su/rockpaperscisors/GameActivity.java
@@ -0,0 +1,158 @@
+package th.ac.su.rockpaperscisors;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+import java.util.Random;
+
+public class GameActivity extends AppCompatActivity  implements View.OnClickListener {
+    private int round = 1, score = 0;
+    private Button[] mButtons = new Button[3];
+    private TextView roundText, challengeText;
+    private int answer[] = new int[3];
+    private String answerStr[] = {"rock", "paper", "scisors"};
+    private String challengeStr[] = {"You turn", "Cool?", "You turn", "Guess What?", "Hey!"};
+    private int indexAnswer, indexChallenge;
+    private char[] result = new char[6];
+    private Random random;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        roundText = findViewById(R.id.round_text);
+        challengeText = findViewById(R.id.challenge_text);
+
+        mButtons[0] = findViewById(R.id.rock_btn);
+        mButtons[1] = findViewById(R.id.paper_btn);
+        mButtons[2] = findViewById(R.id.scissors_btn);
+
+        mButtons[0].setOnClickListener(this);
+        mButtons[1].setOnClickListener(this);
+        mButtons[2].setOnClickListener(this);
+
+        answer[0] = R.id.rock_btn;
+        answer[1] = R.id.paper_btn;
+        answer[2] = R.id.scissors_btn;
+
+        random = new Random();
+
+        game();
+    }
+
+    public void game() {
+        if (round <= 5)
+            roundText.setText("Round " + String.valueOf(round));
+        indexAnswer = random.nextInt(answer.length);
+        indexChallenge = random.nextInt(challengeStr.length);
+        challengeText.setText(challengeStr[indexChallenge]);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Button b = findViewById(v.getId());
+        int comId = answer[indexAnswer];
+        int id = v.getId();
+        String display = "";
+
+        if (id == comId) {
+            display = "draw";
+            score++;
+            result[round] = 'd';
+        } else {
+            if (id == answer[0]) {
+                //rock
+                if (comId == answer[1]) {
+                    //paper
+                    display = "lose";
+                } else if (comId == answer[2]) {
+                    //scisors
+                    display = "win";
+                }
+            } else if (id == answer[1]) {
+                //paper
+                if (comId == answer[0]) {
+                    //rock
+                    display = "win";
+                } else if (comId == answer[2]) {
+                    //scisors
+                    display = "lose";
+                }
+            } else if (id == answer[2]) {
+                //scisors
+                if (comId == answer[0]) {
+                    //rock
+                    display = "lose";
+                } else if (comId == answer[1]) {
+                    //paper
+                    display = "win";
+                }
+            }
+        }
+        if (display.equals("win")) {
+            result[round] = 'w';
+            score += 2;
+        }
+        if(display.equals("lose")) {
+            result[round] = 'l';
+        }
+
+
+        int idUser = 0;
+        int count = 0;
+        for(Integer i : answer){
+            if(i==id) {
+                idUser = count;
+                break;
+            }
+            count++;
+        }
+        AlertDialog.Builder dialog = new AlertDialog.Builder(GameActivity.this);
+        dialog.setTitle("Your " + display);
+        dialog.setMessage("You try :" + answerStr[idUser]  +
+                "\nCompetitor try : " + answerStr[indexAnswer] + "\n your score = " + score);
+        dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (round > 5) {
+                    Intent intent = new Intent(GameActivity.this, Summary.class);
+                    intent.putExtra("score", score);
+                    intent.putExtra("r1", result[1]);
+                    intent.putExtra("r2", result[2]);
+                    intent.putExtra("r3", result[3]);
+                    intent.putExtra("r4", result[4]);
+                    intent.putExtra("r5", result[5]);
+                    startActivity(intent);
+                }
+            }
+        });
+        dialog.show();
+
+
+        if (round <= 5) {
+            round++;
+            game();
+        }
+
+    }
+}
---
+++ app/src/main/java/th/ac/su/rockpaperscisors/Summary.java
@@ -0,0 +1,72 @@
+package th.ac.su.rockpaperscisors;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class Summary extends AppCompatActivity {
+    TextView[] textRound = new TextView[5];
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summary);
+
+    String[] nameResult = new String[5];
+    String[] rX = {"r1","r2","r3","r4","r5"};
+    char[] variable = new char[5];
+
+    textRound[0] = findViewById(R.id.r1_result);
+        textRound[1] = findViewById(R.id.r2_result);
+        textRound[2] = findViewById(R.id.r3_result);
+        textRound[3] = findViewById(R.id.r4_result);
+        textRound[4] = findViewById(R.id.r5_result);
+
+
+        Intent i = getIntent();
+        int score = i.getIntExtra("score",0);
+        TextView scoreTextView = findViewById(R.id.score_text_view);
+        scoreTextView.setText("Your score is.."+String.valueOf(score));
+
+
+
+
+        for(int j = 0;j<5;j++){
+            variable[j] = i.getCharExtra(rX[j],'d');
+        }
+        for(int j = 0;j<5;j++){
+            char c = variable[j];
+            int color =Color.BLACK;
+            String result=" ";
+            if(c=='d'){
+                result =  "draw";
+                color = Color.BLUE;
+            }else if(c=='w'){
+                result = "win";
+                color = Color.GREEN;
+            }else if(c == 'l'){
+                result = "lose";
+                color = Color.RED;
+            }
+            textRound[j].setText(result);
+            textRound[j].setTextColor(color);
+        }
+
+
+
+        Button backButton = findViewById(R.id.back_btn);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Summary.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
